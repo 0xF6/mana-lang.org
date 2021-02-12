d95f575aff52594e6e5fdcd9efbef4c4c5ec41c4
Yuuki Wesp <ls-micro@ya.ru>
2021-02-13 01:40:19 +0300

    fix build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withImages = require("next-images");
 const withSass = require('@zeit/next-sass');
 const withFonts = require('nextjs-fonts');
+const withPlugins = require('next-compose-plugins');
 module.exports = withPlugins([
   withSass({
     cssModules: true
