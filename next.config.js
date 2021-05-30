63018248b889f622823fb4e32e9893f7ca8c874a
Elnar Ibragimov <djelnar@gmail.com>
2021-05-30 23:04:03 +0300

    u

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
-const withImages = require("next-images");
-const withSass = require('@zeit/next-sass');
-const withFonts = require('nextjs-fonts');
-const withPlugins = require('next-compose-plugins');
+const withImages = require('next-images')
+const withFonts = require('nextjs-fonts')
+const withPlugins = require('next-compose-plugins')
 module.exports = withPlugins([
-  withSass({
-    cssModules: true
-  }), 
   withImages({
     reactStrictMode: true,
-  }), 
-  withFonts()
-]);
+  }),
+  withFonts(),
+])
