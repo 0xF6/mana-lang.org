c925fd0150cf372fe24a1d10301d3365d7434116
Elnar Ibragimov <djelnar@gmail.com>
2021-07-14 01:25:38 +0300

    Features

--- src/features-list/index.tsx
+++ src/features-list/index.tsx
@@ -1,3 +1,4 @@
+import classnames from 'classnames'
 import Link from 'next/link'
 import React from 'react'
 import styles from './features-list.module.css'
@@ -10,8 +11,15 @@ export const FeaturesList = () => {
           <h3 className={styles.h3}>Build it in Mana</h3>
           <div className={styles.row}>
             <div className={styles.col}>
+              <div className={classnames(styles.bgWrapper, styles.bgWrapper1)}>
+                <img src="/cli.svg" />
+              </div>
               <div className={styles.imgWrapper}>
-                <img src="/icons/command-line.png" className={styles.img} alt="" />
+                <img
+                  src="/icons/command-line.svg"
+                  className={styles.img}
+                  alt=""
+                />
               </div>
               <p className={styles.desc}>Command Line</p>
               <div className={styles.listWrapper}>
@@ -20,9 +28,13 @@ export const FeaturesList = () => {
                 </Link>
               </div>
             </div>
+
             <div className={styles.col}>
+              <div className={classnames(styles.bgWrapper, styles.bgWrapper2)}>
+                <img src="/embedded.svg" />
+              </div>
               <div className={styles.imgWrapper}>
-                <img src="/icons/cpu.png" className={styles.img} alt="" />
+                <img src="/icons/cpu.svg" className={styles.img} alt="" />
               </div>
               <p className={styles.desc}>Embedded</p>
               <div className={styles.listWrapper}>
@@ -32,8 +44,11 @@ export const FeaturesList = () => {
               </div>
             </div>
             <div className={styles.col}>
+              <div className={classnames(styles.bgWrapper, styles.bgWrapper3)}>
+                <img src="/ai.svg" />
+              </div>
               <div className={styles.imgWrapper}>
-                <img src="/icons/brain.png" className={styles.img} alt="" />
+                <img src="/icons/brain.svg" className={styles.img} alt="" />
               </div>
               <p className={styles.desc}>AI</p>
               <div className={styles.listWrapper}>
@@ -43,8 +58,11 @@ export const FeaturesList = () => {
               </div>
             </div>
             <div className={styles.col}>
+              <div className={classnames(styles.bgWrapper, styles.bgWrapper4)}>
+                <img src="/quantum.svg" />
+              </div>
               <div className={styles.imgWrapper}>
-                <img src="/icons/science.png" className={styles.img} alt="" />
+                <img src="/icons/science.svg" className={styles.img} alt="" />
               </div>
               <p className={styles.desc}>Quantum Computing</p>
               <div className={styles.listWrapper}>
